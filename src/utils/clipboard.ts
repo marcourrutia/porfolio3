647866aadc402e64081f25e8f9f432c71b6ee3a2
marcourrutia <m.urrutia1991@gmail.com>
2025-02-13 22:34:56 -0300

    no funciona button copy

--- src/utils/clipboard.ts
+++
@@ -1,9 +0,0 @@
-export function copyToClipboard(event: MouseEvent, text: string) {
-    event.preventDefault();
-    alert("copiado")
-    console.log("copiado")
-    /* navigator.clipboard
-      .writeText(text)
-      .then(() => alert("¡Correo copiado al portapapeles! 🚀"))
-      .catch((err) => console.error("Error al copiar: ", err)); */
-  }
